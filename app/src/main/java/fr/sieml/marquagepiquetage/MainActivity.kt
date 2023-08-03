c87483a8eb72d9a8f943f9829f8cc68a090859ad
TLBail <62939298+TLBail@users.noreply.github.com>
2023-08-03 15:11:15 +0200

    add untracked files

---
+++ app/src/main/java/fr/sieml/marquagepiquetage/MainActivity.kt
@@ -0,0 +1,41 @@
+package fr.sieml.marquagepiquetage
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.sieml.marquagepiquetage.ui.theme.MarquagePiquetageTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+                }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
+
+        setContent {
+            MarquagePiquetageTheme {
+                MarquagePiquetageNavHost()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/fr/sieml/marquagepiquetage/Signature/DrawMode.kt
@@ -0,0 +1,5 @@
+package fr.sieml.marquagepiquetage.Signature
+
+enum class DrawMode {
+    Draw, Touch, Erase
+}
