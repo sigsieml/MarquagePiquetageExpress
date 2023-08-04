1c7a1b9e82f944aef1da1e4d420c0a7fa5d05ced
TLBail <62939298+TLBail@users.noreply.github.com>
2023-08-04 15:42:56 +0200

    fix image location

--- app/src/main/java/fr/sieml/marquagepiquetage/MainActivity.kt
+++ app/src/main/java/fr/sieml/marquagepiquetage/MainActivity.kt
@@ -1,7 +1,9 @@
 package fr.sieml.marquagepiquetage
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +12,6 @@ import fr.sieml.marquagepiquetage.ui.theme.MarquagePiquetageTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         val locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
@@ -21,15 +22,26 @@ class MainActivity : AppCompatActivity() {
                 }
                 permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                     // Only approximate location access granted.
-                } else -> {
-                // No location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.WRITE_EXTERNAL_STORAGE, false) -> {
+                    // Write permission granted
+                }
+                permissions.getOrDefault(Manifest.permission.READ_EXTERNAL_STORAGE, false) -> {
+                    // Read permission granted
+                }
+                else -> {
+                    // Permission denied.
+                    Toast.makeText(this, "L'application a besoin des permissions pour fonctionner", Toast.LENGTH_SHORT).show()
+                    finish()
                 }
             }
         }
 
         locationPermissionRequest.launch(arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE))
 
 
         setContent {
--- app/src/main/java/fr/sieml/marquagepiquetage/pdf/PhotoUriManager.kt
+++ app/src/main/java/fr/sieml/marquagepiquetage/pdf/PhotoUriManager.kt
@@ -16,31 +16,49 @@
 
 package fr.sieml.marquagepiquetage.pdf
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 /**
  * Manages the creation of photo Uris. The Uri is used to store the photos taken with camera.
  */
 class PhotoUriManager(private val appContext: Context) {
-
     fun buildNewUri(): Uri {
-        val photosDir = File(appContext.cacheDir, PHOTOS_DIR)
-        photosDir.mkdirs()
-        val photoFile = File(photosDir, generateFilename())
-        val authority = "${appContext.packageName}.$FILE_PROVIDER"
-        return FileProvider.getUriForFile(appContext, authority, photoFile)
+        // Add a specific media item.
+        val resolver: ContentResolver = appContext.getContentResolver()
+
+// Find all audio files on the primary external storage device.
+        val imageCollection: Uri
+        imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media
+                .getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        // Générer un nom de fichier unique pour la photo
+        val timeStamp =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                .format(Date())
+        val fileName = "IMG_$timeStamp.jpg"
+        val newImageDetails = ContentValues()
+        newImageDetails.put(
+            MediaStore.Images.Media.DISPLAY_NAME,
+            fileName
+        )
+        newImageDetails.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        return resolver
+            .insert(imageCollection, newImageDetails)!!
     }
 
-    /**
-     * Create a unique file name based on the time the photo is taken
-     */
-    private fun generateFilename() = "selfie-${System.currentTimeMillis()}.jpg"
 
-    companion object {
-        private const val PHOTOS_DIR = "photos"
-        private const val FILE_PROVIDER = "fileprovider"
-    }
 }
